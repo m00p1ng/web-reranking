7fae2b76f05cd4fe562257c5ab7dc1bc61ec9ccb
mooping <mongkonchai4412@gmail.com>
2017-10-23 22:48:49 +0700

    add urlmap with shellscript

---
+++ webGraph/urlmap.sh
@@ -0,0 +1,9 @@
+HTML_SRC="$HOME/Desktop/ir_proj/html"
+DESC="$HOME/Desktop"
+RESULT="$DESC/urlmap.txt"
+
+pat=$(echo $HTML_SRC | sed -e 's/[]\/$*.^|[]/\\&/g')
+
+find $HTML_SRC -type f |\
+sed -e "s/^$pat\///g" |\
+sort > $RESULT
