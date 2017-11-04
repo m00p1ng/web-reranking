12e4beeec8becedf586f62965aff939cdb8e9d41
mooping <mongkonchai4412@gmail.com>
2017-11-05 00:00:23 +0700

    add write file function

--- webGraph/main.go
+++ webGraph/main.go
@@ -1,10 +1,51 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
 	"./webgraph"
 )
 
 func main() {
 	urlMap := webgraph.Urlmap(htmlPath)
+	writeFile("urlmap.txt", func(file io.Writer) {
+		for _, url := range urlMap {
+			fmt.Fprintf(file, "%s\n", url)
+		}
+	})
+
 	webgraph.GetGraph(urlMap, tags)
+}
+
+func writeFile(fn string, h func(io.Writer)) {
+	cur, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	outputPath := filepath.Join(cur, "output")
+	outputFile := filepath.Join(outputPath, fn)
+
+	if isNotExist(outputPath) {
+		os.Mkdir(outputPath, 0700)
+	}
+
+	file, err := os.Create(outputFile)
+
+	if err != nil {
+		panic(err)
+	}
+
+	h(file)
+	file.Close()
+}
+
+func isNotExist(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return true
+	}
+	return false
 }
--- webGraph/webgraph/utils.go
+++ webGraph/webgraph/utils.go
@@ -5,14 +5,12 @@ import (
 	"path/filepath"
 )
 
-type handler func(string)
-
 func isDirectory(p string) (bool, error) {
 	fileInfo, err := os.Stat(p)
 	return fileInfo.IsDir(), err
 }
 
-func pathTraverse(p string, h handler) {
+func pathTraverse(p string, h func(string)) {
 	paths, err := filepath.Glob(p + "/*")
 	if err != nil {
 		panic(err)
